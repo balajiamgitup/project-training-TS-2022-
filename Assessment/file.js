a1f9840492f2cf2dce98f8f801a4a571b592a52b
balajiamgitup <104089954+balajiamgitup@users.noreply.github.com>
2022-07-25 17:03:56 +0530

    Delete Assessment directory

--- Assessment/file.js
+++
@@ -1,5 +0,0 @@
-var file=require("fs");
-var filedata=file.readFileSync("order_detals.txt");
-var data=JSON.parse(filedata);
-console.log(data)
- module.exports={data};
--- Assessment/prompt.test.js
+++
@@ -1,9 +0,0 @@
-const restorent=require("./retorentLevel1");
-jest.mock('prompt-sync', () => () => () => "Idlli");
-describe("User activities ",()=>
-{
- test("Taking order",()=>{
-     expect(restorent.obj.iItemList()).toStrictEqual(["Idlli"]);
- })
-
-});
